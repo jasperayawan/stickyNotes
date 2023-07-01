a396bbad611c79d67855052377e1c4f2aac9e22a
jasperayawan <ejayawan22@gmail.com>
2023-07-02 03:47:00 +0800

    update name

--- client/src/pages/layout.jsx
+++ client/src/pages/layout.jsx
@@ -18,7 +18,7 @@ export default function Layout(props) {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl">Stinky-Notes</span>
+            <span className="ml-3 text-xl">Sticky-Notes</span>
           </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             {user ? (
